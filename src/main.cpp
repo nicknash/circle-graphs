a21fa45515a7542e56251297ad6fc2e77c2bc839
Nicholas Nash <nobody@here>
2025-03-15 16:57:18 +0400

    get interactive debugging working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+
 #include "data_structures/interval.h"
 #include "data_structures/simple_interval_rep.h"
 #include "utils/interval_rep_utils.h"
 
+#include "mis/naive.h"
 #include "mis/switching.h"
 
 int main()
@@ -10,8 +12,7 @@ int main()
     auto intervals = cg::utils::generate_random_intervals(1000);
 
     auto intervalRep = cg::data_structures::SimpleIntervalRep(intervals);
-    auto density = cg::utils::compute_density(intervalRep);
-    std::cout << "density: " << density << std::endl;
 
+    cg::mis::Naive::computeMIS(intervalRep);
     return 0;
 }
