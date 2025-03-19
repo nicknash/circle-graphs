6c11ecff03d1747546077f68f283ab1d872e84eb
Nicholas Nash <nobody@here>
2025-03-19 18:54:26 +0400

    extremely early stub of mif algorithm

---
+++ include/mif/gavril.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <span>
+
+#include "data_structures/simple_interval_rep.h"
+
+namespace cg::data_structures
+{
+    class SimpleIntervalRep;
+    class Interval;
+}
+
+namespace cg::mif
+{
+    class Gavril
+    {
+    public:
+        static void computeMif(std::span<const Interval> intervals)
+        {
+            // sort intervals by length.
+            auto levelEndIndex = 123; // index one past end of shortest intervals
+            
+            auto levelNumber = 0;
+
+            cg::data_structures::SimpleIntervalRep intervalRep(intervals);
+            while(levelEndIndex < intervals.size())
+            {
+                for(auto x = 0; x < intervalRep.end; x++)
+                {
+                    for(auto y = x + 1; y < intervals.)
+                }
+
+                ++levelNumber;
+                // advance levelEndIndex to one past end of next length category
+            }
+        }
+    };
+}
---
+++ src/mif/gavril.cpp
@@ -0,0 +1,52 @@
+
+#include "data_structures/simple_interval_rep.h"
+#include "data_structures/interval.h"
+
+#include "mif/gavril.h"
+
+namespace cg::mif
+{
+    static void computeMif(std::span<const cg::data_structures::Interval> intervals)
+    {
+        // sort intervals by length.
+        auto levelEndIndex = 123; // index one past end of shortest intervals
+
+        auto levelNumber = 0;
+
+        cg::data_structures::SimpleIntervalRep intervalRep(intervals);
+        while (levelEndIndex < intervals.size())
+        {
+            auto levelStartIndex = 3343;
+
+            for (auto intervalIdx = levelStartIndex; intervalIdx < levelEndIndex; ++intervalIdx)
+            {
+                const auto& w_interval = intervals[intervalIdx];
+
+
+                // may not want these loops: ? does the "The right endpoints r_w .. " begin to describe the mechanism for the computation of FR_w,i FL_w,i ?
+                for (auto x = 0; x < intervalRep.end; x++)
+                {
+                    for (auto y = x + 1; y < intervalRep.end; ++y)
+                    {
+                        // if l_w < x and r_w \in [x, y]
+                        if(w_interval.Left < x && x <= w_interval.Right && w_interval.Right <= y)
+                        {
+                            // Construct FR_w,i[x, y] (note we call i levelNumber) 
+
+                        }
+
+                        // if y < r_w and l_w \in [x, y] 
+                        if(y < w_interval.Right && x <= w_interval.Left && w_interval.Left <= y)
+                        {
+                            // Construct FL_w,i[x,y] (note we call i levelNumber)
+                        }
+                    
+                    }
+                }
+            }
+
+            ++levelNumber;
+            // advance levelEndIndex to one past end of next length category
+        }
+    }
+}
