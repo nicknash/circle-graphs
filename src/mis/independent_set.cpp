24f9aba9e32b1322570c4bf6ff07f819cbf6d832
Nicholas Nash <nobody@here>
2025-04-09 12:49:44 +0400

    add a comment explaining linear space

--- src/mis/independent_set.cpp
+++ src/mis/independent_set.cpp
@@ -18,13 +18,15 @@ namespace cg::mis
 
     void IndependentSet::setNewNextInterval(int where, const cg::data_structures::Interval& interval) 
     {
-        if(where == 0 && interval.Left == 2 && interval.Right == 5)
-        {
-            _endpointToInterval[where] = interval;
-        }
         _endpointToInterval[where] = interval;
     }
 
+    // It's worth a quick explanation of the space complexity implied by calling assembleContainedIndependentSet for each of k intervals.
+    // This results in, for each of the k intervals, the set of intervals directly contained in that interval being stored. Directly contained in an interval I, means intervals that are contained 
+    // in I, but not in any other interval that is contained in I.
+    //
+    // This requires, for all the k intervals only O(k) space. The reason is that for any interval, it can only be directly contained in at most 2 other intervals (because, if there was a third
+    // interval containing any interval, it would have to contain one of the other two, violating direct containment)
     void IndependentSet::assembleContainedIndependentSet(const cg::data_structures::Interval &interval)
     {
         std::optional<cg::data_structures::Interval> maybeNext = _endpointToInterval[interval.Left + 1];
