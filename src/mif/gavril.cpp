b2e09da664e9855a066941fb1e3964286958ef75
Nicholas Nash <nobody@here>
2025-09-03 19:17:49 +0100

    add stub of Gavril's MIF algorithm using the new layers algorithm

--- src/mif/gavril.cpp
+++ src/mif/gavril.cpp
@@ -10,47 +10,14 @@ namespace cg::mif
 {
     static void computeMif(std::span<const cg::data_structures::Interval> intervals)
     {
-        // No, we actually want the containment DAG here.
+        const cg::data_structures::DistinctIntervalModel intervalModel(intervals);
+        const auto& layers = cg::interval_model_utils::createLayers(intervalModel);
 
-        // sort intervals by length.
-        auto levelEndIndex = 123; // index one past end of shortest intervals
-
-        auto levelNumber = 0;
-
-        cg::data_structures::DistinctIntervalModel intervalModel(intervals);
-        while (levelEndIndex < intervals.size())
+        // The 'layerIdx' is 'i' from Gavril's paper, that the induction of Theorem 5 is on,
+        // and what FR_{w, i}, HR_{w, i}, etc are defined on.
+        for(int layerIdx = 1; layerIdx < layers.size(); ++layerIdx)
         {
-            auto levelStartIndex = 3343;
 
-            for (auto intervalIdx = levelStartIndex; intervalIdx < levelEndIndex; ++intervalIdx)
-            {
-                const auto& w_interval = intervals[intervalIdx];
-
-
-                // may not want these loops: ? does the "The right endpoints r_w .. " begin to describe the mechanism for the computation of FR_w,i FL_w,i ?
-                for (auto x = 0; x < intervalModel.end; x++)
-                {
-                    for (auto y = x + 1; y < intervalModel.end; ++y)
-                    {
-                        // if l_w < x and r_w \in [x, y]
-                        if(w_interval.Left < x && x <= w_interval.Right && w_interval.Right <= y)
-                        {
-                            // Construct FR_w,i[x, y] (note we call i levelNumber) 
-
-                        }
-
-                        // if y < r_w and l_w \in [x, y] 
-                        if(y < w_interval.Right && x <= w_interval.Left && w_interval.Left <= y)
-                        {
-                            // Construct FL_w,i[x,y] (note we call i levelNumber)
-                        }
-                    
-                    }
-                }
-            }
-
-            ++levelNumber;
-            // advance levelEndIndex to one past end of next length category
         }
     }
 }
