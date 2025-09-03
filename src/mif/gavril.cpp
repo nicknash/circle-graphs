c0fc78c443562dcf33989779388d42441235ac55
Nicholas Nash <nobody@here>
2025-09-03 19:57:45 +0100

    initial outline of implementation

--- src/mif/gavril.cpp
+++ src/mif/gavril.cpp
@@ -8,16 +8,31 @@
 
 namespace cg::mif
 {
+    // This is Gavril's algorithm for the maximum induced forest of a circle graph:
+    // "Minimum weight feedback vertex sets in circle graphs", Information Processing Letters 107 (2008),pp1-6
     static void computeMif(std::span<const cg::data_structures::Interval> intervals)
     {
         const cg::data_structures::DistinctIntervalModel intervalModel(intervals);
+        // The 'layers' are what Gavril calls A_0, ..., A_k at the start of page 5.
         const auto& layers = cg::interval_model_utils::createLayers(intervalModel);
 
+        const auto& firstLayer = layers[0];
+        // TODONICK: Implement the base cases.
+
+
+        auto layersSoFar = firstLayer;
         // The 'layerIdx' is 'i' from Gavril's paper, that the induction of Theorem 5 is on,
         // and what FR_{w, i}, HR_{w, i}, etc are defined on.
         for(int layerIdx = 1; layerIdx < layers.size(); ++layerIdx)
         {
-
+            // 'currentLayer' is V_k in Gavril's notation, and is the concatenation of this and all previous layers
+            const auto& newLayer = layers[layerIdx];
+            layersSoFar.insert(layersSoFar.end(), newLayer.begin(), newLayer.end());
+        
+            // Evaluate FL, FR for intervals in newLayer
+            // Do the right-to-left scan for all intervals in currentLayer
+            // Do the left-to-right scan
+            // Calculate the MWIS representative
         }
     }
 }
